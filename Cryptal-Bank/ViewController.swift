410a52cea5d4724d9deee17dfb72f97c4f5ad545
ikakooo <i_chkhitunidze@cu.edu.ge>
2021-12-10 15:54:03 +0400

    init ios project

---
+++ Cryptal-Bank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cryptal-Bank
+//
+//  Created by MacBook Pro on 10.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
