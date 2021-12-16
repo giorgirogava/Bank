c4bbd736332538098d5476de3539da2b63f77dc1
ikakooo <i_chkhitunidze@cu.edu.ge>
2021-12-16 13:06:37 +0400

    intro scenes started

---
+++ Cryptal-Bank/Presentation/Scenes/Intro scene/AppIntroViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AppIntroViewController.swift
+//  Cryptal-Bank
+//
+//  Created by MacBook Pro on 16.12.21.
+//
+
+import UIKit
+
+class AppIntroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
