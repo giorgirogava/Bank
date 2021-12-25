8f4934347211d3ddf1baf89f692591030b174149
ikakooo <i_chkhitunidze@cu.edu.ge>
2021-12-25 17:44:50 +0400

    tab bar controller added

--- Cryptal-Bank/Presentation/Scenes/Log in Scene/LogInViewController.swift
+++ Cryptal-Bank/Presentation/Scenes/Log in Scene/LogInViewController.swift
@@ -15,15 +15,15 @@ class LogInViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func onLoginButtonClick(_ sender: Any) {
+        
+        navigationController?.popToRootViewController( animated: false )
+        let sb = UIStoryboard(name: "MainDashboardTabBarController", bundle: nil)
+        let vc = sb.instantiateViewController(withIdentifier: "MainDashboardTabBarController")
+        
+        navigationController?.viewControllers.removeAll()
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true)
     }
-    */
 
 }
---
+++ Cryptal-Bank/Presentation/Scenes/Main Dashboard/MainDashboardTabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  MainDashboardTabBarController.swift
+//  Cryptal-Bank
+//
+//  Created by MacBook Pro on 25.12.21.
+//
+
+import UIKit
+
+class MainDashboardTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
