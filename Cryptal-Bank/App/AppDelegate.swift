ff5caf4e704c1df49242ccd756ee4f317b9d3f5c
giorgirogava <giorgirogava01ai@gmail.com>
2021-12-25 20:28:40 +0400

    - feat: add transaction notification

--- Cryptal-Bank/App/AppDelegate.swift
+++ Cryptal-Bank/App/AppDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 import CoreData
 import Firebase
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) {
+        
+            (granted, error) in if granted {
+                print("User gave permissions for local notifications")
+            }
+            
+        }
+        
         return true
     }
 
