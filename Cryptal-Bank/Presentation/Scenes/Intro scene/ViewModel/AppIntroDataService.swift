82772b2400106683be2dd6f9c28910ab9cc239a5
ikakooo <i_chkhitunidze@cu.edu.ge>
2021-12-27 03:17:11 +0400

    auto scroll with bug

--- Cryptal-Bank/Presentation/Scenes/Intro scene/ViewModel/AppIntroDataService.swift
+++ Cryptal-Bank/Presentation/Scenes/Intro scene/ViewModel/AppIntroDataService.swift
@@ -32,7 +32,7 @@ class AppIntroDataService: NSObject, UICollectionViewDataSource {
     func refresh() {
         pages = viewModel.getPages()
         collectionView.reloadData()
-       // startTimer()
+        startTimer()
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -48,30 +48,30 @@ class AppIntroDataService: NSObject, UICollectionViewDataSource {
     
     func startTimer() {
 
-        _ =  Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(self.scrollAutomatically), userInfo: nil, repeats: true)
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.scrollAutomatically), userInfo: nil, repeats: true)
     }
     
     @objc func scrollAutomatically(_ timer1: Timer) {
-
-        if let coll  = collectionView {
-            for cell in coll.visibleCells {
-                let indexPath: IndexPath? = coll.indexPath(for: cell)
-                
-              //  if ((indexPath?.row)! < pages.count + 1){
-                    let indexPath1: IndexPath?
-                    indexPath1 = IndexPath.init(row:  2, section: (indexPath?.section)!)
-                   print("ikakoo")
-                coll.scrollToItem(at: indexPath1!, at: .left, animated: true)
-                    pageDidChanged?((indexPath?.row)!)
-               // }
-//                else{
-//                    let indexPath1: IndexPath?
-//                    indexPath1 = IndexPath.init(row: 0, section: (indexPath?.section)!)
-//                    coll.scrollToItem(at: indexPath1!, at: .right, animated: true)
-//                }
-
+            
+            if let coll  = collectionView {
+                for cell in coll.visibleCells {
+                    let indexPath: IndexPath? = coll.indexPath(for: cell)
+                    if ((indexPath?.row)!  < pages.count - 1){
+                        let indexPath1: IndexPath?
+                        indexPath1 = IndexPath.init(row: (indexPath?.row)! + 1, section: (indexPath?.section)!)
+                        
+                        coll.scrollToItem(at: indexPath1!, at: .centeredHorizontally, animated: true)
+                    }
+                    else{
+                        let indexPath1: IndexPath?
+                        indexPath1 = IndexPath.init(row: 0, section: (indexPath?.section)!)
+                        coll.scrollToItem(at: indexPath1!, at: .right, animated: true)
+                    }
+                    
+                }
             }
-        }}
+            
+        }
 
 }
 
