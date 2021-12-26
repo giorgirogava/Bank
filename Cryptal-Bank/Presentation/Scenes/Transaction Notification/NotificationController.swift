e48263fe517f5cbabc36c6883497e47bfed7c16a
giorgirogava <giorgirogava01ai@gmail.com>
2021-12-26 21:47:43 +0400

    - feat: add transaction notification

--- Cryptal-Bank/Presentation/Scenes/Transaction Notification/NotificationController.swift
+++ Cryptal-Bank/Presentation/Scenes/Transaction Notification/NotificationController.swift
@@ -10,32 +10,33 @@ class NotificationController: UIViewController {
         
     }
     
-    @IBAction func setReminder(_ sender: Any) {
+  
+    @IBAction func setReminder(_ sender: UIButton) {
         
-    
-        let center = UNUserNotificationCenter.current()
         
-        let content = UNMutableNotificationContent()
-        
-        content.title = "TBC Bank"
-        
-        content.body = "Hi, it's TBC Bank We're glad to inform you that we have confirmed your payment. Thank you for using our service. "
-        
-        content.sound = .default
-        
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 7, repeats: false)
-        
-        let request = UNNotificationRequest(identifier: "reminder", content: content, trigger: trigger)
-        
-        center.add(request) {
-            (error) in
-            if error != nil {
-                print("Error = \(error?.localizedDescription ?? "error local notification")")
+            let center = UNUserNotificationCenter.current()
+            
+            let content = UNMutableNotificationContent()
+            
+            content.title = "TBC Bank"
+            
+            content.body = "Hi, it's TBC Bank We're glad to inform you that we have confirmed your payment. Thank you for using our service. "
+            
+            content.sound = .default
+            
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 7, repeats: false)
+            
+            let request = UNNotificationRequest(identifier: "reminder", content: content, trigger: trigger)
+            
+            center.add(request) {
+                (error) in
+                if error != nil {
+                    print("Error = \(error?.localizedDescription ?? "error local notification")")
+                }
+                
+                
             }
-            
-            
-        }
-        
     }
+    
     
 }
