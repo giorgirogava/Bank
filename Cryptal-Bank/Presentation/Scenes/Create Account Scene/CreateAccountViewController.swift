78bd01a7b376a2d6cadcb0d7c51d469899bd224e
ikakooo <i_chkhitunidze@cu.edu.ge>
2022-01-02 02:43:17 +0400

    change main app icon

--- Cryptal-Bank/Presentation/Scenes/Create Account Scene/CreateAccountViewController.swift
+++ Cryptal-Bank/Presentation/Scenes/Create Account Scene/CreateAccountViewController.swift
@@ -15,15 +15,9 @@ class CreateAccountViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func onLoginClick(_ sender: Any) {
+//        dismiss(animated: true, completion: nil)
+        navigationController?.popViewController(animated: true)
     }
-    */
 
 }
