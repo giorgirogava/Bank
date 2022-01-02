c971bd9a09e985bd9487483ca0e014eaf40612b0
giorgirogava <giorgirogava01ai@gmail.com>
2022-01-02 18:06:51 +0400

    - feat: add widgets to the home screen

---
+++ Cryptal-Bank-Widget/Cryptal_Bank_Widget.swift
@@ -0,0 +1,67 @@
+//
+//  Cryptal_Bank_Widget.swift
+//  Cryptal-Bank-Widget
+//
+//  Created by IMAC on 02.01.22.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date())
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date())
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate)
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+}
+
+struct Cryptal_Bank_WidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Text(entry.date, style: .time)
+    }
+}
+
+@main
+struct Cryptal_Bank_Widget: Widget {
+    let kind: String = "Cryptal_Bank_Widget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            Cryptal_Bank_WidgetEntryView(entry: entry)
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+struct Cryptal_Bank_Widget_Previews: PreviewProvider {
+    static var previews: some View {
+        Cryptal_Bank_WidgetEntryView(entry: SimpleEntry(date: Date()))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
