2c179f85c7788054b7e23a0bc43f2a3de364c3fc
giorgirogava <giorgirogava01ai@gmail.com>
2022-01-04 18:40:54 +0400

    - feat: add photo widget

--- Cryptal-Bank-Widget/Cryptal_Bank_Widget.swift
+++ Cryptal-Bank-Widget/Cryptal_Bank_Widget.swift
@@ -12,15 +12,15 @@ struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date())
     }
-
+    
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
         let entry = SimpleEntry(date: Date())
         completion(entry)
     }
-
+    
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
-
+        
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
@@ -28,7 +28,7 @@ struct Provider: TimelineProvider {
             let entry = SimpleEntry(date: entryDate)
             entries.append(entry)
         }
-
+        
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
@@ -40,16 +40,36 @@ struct SimpleEntry: TimelineEntry {
 
 struct Cryptal_Bank_WidgetEntryView : View {
     var entry: Provider.Entry
-
+    
     var body: some View {
-        Text(entry.date, style: .time)
+        ZStack{
+            
+            
+            GeometryReader{ geo in
+                
+                Image("AppIcon-1").resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: geo.size.width,
+                           height: geo.size.height, alignment: .center)
+                
+            }
+            
+            
+            Text(entry.date, style: .time)
+                .font(Font.system(size: 24, weight: .semibold, design: .default))
+                .foregroundColor(Color.white)
+            
+        }
+        
     }
+    
 }
+
 
 @main
 struct Cryptal_Bank_Widget: Widget {
     let kind: String = "Cryptal_Bank_Widget"
-
+    
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             Cryptal_Bank_WidgetEntryView(entry: entry)
