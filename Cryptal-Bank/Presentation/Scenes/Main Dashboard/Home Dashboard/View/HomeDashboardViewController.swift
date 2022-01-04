c22a54e64a076db95000bd0e4c8e4ceae608e35e
ikakooo <i_chkhitunidze@cu.edu.ge>
2022-01-04 04:23:04 +0400

    HomeDashboard transformed MVC to MVVM

--- Cryptal-Bank/Presentation/Scenes/Main Dashboard/Home Dashboard/View/HomeDashboardViewController.swift
+++ Cryptal-Bank/Presentation/Scenes/Main Dashboard/Home Dashboard/View/HomeDashboardViewController.swift
@@ -6,8 +6,6 @@
 //
 
 import UIKit
-import FirebaseAuth
-import FirebaseDatabase
 
 class HomeDashboardViewController: UIViewController {
     @IBOutlet weak private var ballanceLabel: UILabel!
@@ -15,131 +13,26 @@ class HomeDashboardViewController: UIViewController {
     @IBOutlet weak private var eyeButton: UIButton!
     
     private var viewModel: HomeDashboardViewModelProtocol!
-    private var dataService: CreateAccountDataService!
-    
-    let appUserUid = Auth.auth().currentUser?.uid
+    private var dataService: HomeDashboardDataService!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        isHidenLive(){ [unowned self] hiden in
-            if hiden {
-                self.eyeButton.setImage(UIImage(systemName: "eye.slash"), for: .normal)
-                ballanceLabel.text = "$ $ $ $"
-                currencyLabel.isHidden = true
-            }else {
-                self.eyeButton.setImage(UIImage(systemName: "eye"), for: .normal)
-                
-                totalBalanceSingle(){[unowned self] balance in
-                    ballanceLabel.text = balance.moneyFormat()
-                }
-                currencyLabel.isHidden = false
-            }
-        }
-        
-        totalBalanceLive(){[unowned self] balance in
-            
-            ballanceLabel.text = balance.moneyFormat()
-        }
         
         configureDataSource()
     }
     
     private func configureDataSource() {
-        // HomeDashboardViewModel
+        viewModel = HomeDashboardViewModel()
+        dataService = HomeDashboardDataService(withController: self,
+                                               ballanceLabel: ballanceLabel,
+                                               currencyLabel: currencyLabel,
+                                               eyeButton: eyeButton,
+                                               viewModel: viewModel)
+        dataService.loadDashboard()
         
     }
     @IBAction func onEyeButtonClick(_ sender: UIButton) {
-        //        sender.setImage(UIImage(systemName: "eye"), for: .normal)
-        
-        isHidenSingle(){ [unowned self] isHiden in
-            var hiden = isHiden
-            hiden.toggle()
-            balance(hide: hiden)
-        }
+        dataService.changeBalanceHidenStatusTogle()
     }
-    
-    
-    func totalBalanceSingle(balance: @escaping (_ balance: String) -> ()){
-        guard let appUserUid = self.appUserUid else { return}
-        
-        let myNewRef = Database.database(url: Servers.Firebase_Base_URL).reference(withPath:"UsersPrivateData/\(appUserUid)/totalBalance")
-        
-        // myNewRef.setValue("1387213838.234")
-        
-        myNewRef.observeSingleEvent(of: .value, with: { snapshot in
-            if snapshot.exists() {
-                let value =   snapshot.value as? String ?? "0"
-                
-                balance(value)
-            }else {
-                balance("0")
-            }
-        })
-        
-    }
-    
-    func totalBalanceLive(balance: @escaping (_ balance: String) -> ()){
-        guard let appUserUid = self.appUserUid else { return}
-        
-        let myNewRef = Database.database(url: Servers.Firebase_Base_URL).reference(withPath:"UsersPrivateData/\(appUserUid)/totalBalance")
-        
-         myNewRef.setValue("5.234")
-        
-        myNewRef.observe(.value) { snapshot in
-            if snapshot.exists() {
-                let value =   snapshot.value as? String ?? "0"
-                
-                balance(value)
-            }else {
-                balance("0")
-            }
-        }
-        
-    }
-    
-    
-    
-    func balance(hide: Bool){
-        guard let appUserUid = self.appUserUid else { return}
-        
-        let myNewRef = Database.database(url: Servers.Firebase_Base_URL).reference(withPath:"UsersConfigs/\(appUserUid)/balanceIsHiden")
-        
-        myNewRef.setValue( hide )
-    }
-    
-    
-    
-    func isHidenLive( hiden: @escaping (_ hiden: Bool) -> ()){
-        guard let appUserUid = self.appUserUid else { return}
-        
-        let myNewRef = Database.database(url: Servers.Firebase_Base_URL).reference(withPath:"UsersConfigs/\(appUserUid)/balanceIsHiden")
-        
-        myNewRef.observe(.value) { snapshot in
-            if snapshot.exists() {
-                let value =   snapshot.value as? Bool ?? false
-                
-                hiden(value)
-            }else {
-                hiden(false)
-            }
-        }
-    }
-    func isHidenSingle( hiden: @escaping (_ hiden: Bool) -> ()){
-        guard let appUserUid = Auth.auth().currentUser?.uid else { return}
-        
-        let myNewRef = Database.database(url: Servers.Firebase_Base_URL).reference(withPath:"UsersConfigs/\(appUserUid)/balanceIsHiden")
-        
-        myNewRef.observeSingleEvent(of: .value, with: { snapshot in
-            if snapshot.exists() {
-                let value =   snapshot.value as? Bool ?? false
-                
-                hiden(value)
-            }else {
-                hiden(false)
-            }
-        })
-    }
-    
 }
 
--- Cryptal-Bank/Presentation/Scenes/Main Dashboard/Home Dashboard/ViewModel/HomeDashboardDataService.swift
+++ Cryptal-Bank/Presentation/Scenes/Main Dashboard/Home Dashboard/ViewModel/HomeDashboardDataService.swift
@@ -8,7 +8,58 @@
 import UIKit
 
 class HomeDashboardDataService:NSObject {
+     weak private var controller: UIViewController!
+     weak private var viewModel: HomeDashboardViewModelProtocol!
     
+     weak private var ballanceLabel: UILabel!
+     weak private var currencyLabel: UILabel!
+     weak private var eyeButton: UIButton!
+    
+    init(withController: UIViewController,
+         ballanceLabel: UILabel,
+         currencyLabel: UILabel!,
+         eyeButton: UIButton,
+         viewModel: HomeDashboardViewModelProtocol)
+    {
+        super.init()
+        self.controller = withController
+        self.ballanceLabel = ballanceLabel
+        self.currencyLabel = currencyLabel
+        self.eyeButton = eyeButton
+        self.viewModel = viewModel
+    }
+    
+    func loadDashboard(){
+        viewModel.isHidenLive(){ [unowned self] hiden in
+            if hiden {
+                self.eyeButton.setImage(UIImage(systemName: "eye.slash"), for: .normal)
+                ballanceLabel.text = "$ $ $ $"
+                currencyLabel.isHidden = true
+            }else {
+                self.eyeButton.setImage(UIImage(systemName: "eye"), for: .normal)
+                
+                viewModel.totalBalanceSingle(){[unowned self] balance in
+                    ballanceLabel.text = balance.moneyFormat()
+                }
+                currencyLabel.isHidden = false
+            }
+        }
+        
+        viewModel.totalBalanceLive(){[unowned self] balance in
+            
+            ballanceLabel.text = balance.moneyFormat()
+        }
+    }
+    
+    
+    func changeBalanceHidenStatusTogle(){
+        viewModel.isHidenSingle(){ [unowned self] isHiden in
+            var hiden = isHiden
+            hiden.toggle()
+            viewModel.balance(hide: hiden)
+        }
+        
+    }
     
     
     
