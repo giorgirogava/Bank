a6b589d14ab21da961a8bd27dae17575a4bc9161
giorgirogava <giorgirogava01ai@gmail.com>
2022-01-06 18:41:24 +0400

    - feat: add Google Maps

---
+++ Cryptal-Bank/Presentation/Scenes/Main Dashboard/Search Locations/GoogleMapsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GoogleMapsViewController.swift
+//  Cryptal-Bank
+//
+//  Created by IMAC on 04.01.22.
+//
+
+import UIKit
+
+class GoogleMapsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+}
